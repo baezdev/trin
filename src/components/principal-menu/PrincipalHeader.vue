511ad88bb322e4bf8f126cbbffbf4d3665fe0b77
Ángel Baez <baezdev@gmail.com>
2026-01-28 15:58:07 -0600

    add pill info

--- src/components/principal-menu/PrincipalHeader.vue
+++ src/components/principal-menu/PrincipalHeader.vue
@@ -8,6 +8,7 @@ import UiContainer from '../ui/UiContainer.vue'
       <div class="flex items-center py-1.5 gap-3 relative">
         <img src="/images/logo.webp" alt="Logo Trin Ice cream" class="w-14" />
         <h2 class="font-bold text-xl">Trin Ice Cream</h2>
+        <span class="bg-green-200 text-green-700 px-3.5 py-0.5 rounded-full">Proximamente</span>
       </div>
     </UiContainer>
   </header>
